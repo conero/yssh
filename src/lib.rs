bc46e4cd493c3a96ff24a9eb7153b3bf0ef15af3
Conero <conero@163.com>
2019-11-03 23:13:11 +0800

    rust 模块化与文件学习

---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+
+//pub mod variable;
+//mod variable;
+pub const START_DATE: &'static str = "2019-11-01";
+pub const AUTHOR: &'static str = "Joshua Conero";
+pub const VERSION: &'static str = "0.1.0";
+pub const RELEASE: &'static str = "future";
---
+++ src/main.rs
@@ -0,0 +1,8 @@
+//2019年11月1日 星期五
+//uysh 项目主入口
+
+mod lib;
+fn main() {
+    println!("Hello, world!");
+    println!("{}", lib::START_DATE);
+}
