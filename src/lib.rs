b2e07e9435cf755ab74803ceff47f40b45618d59
Conero <conero@163.com>
2019-11-03 23:21:35 +0800

    模块系统学习(rust)

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,5 @@
 
 //pub mod variable;
-//mod variable;
-pub const START_DATE: &'static str = "2019-11-01";
-pub const AUTHOR: &'static str = "Joshua Conero";
-pub const VERSION: &'static str = "0.1.0";
-pub const RELEASE: &'static str = "future";
+
+#[path = "variable.rs"]
+pub mod variable;
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@
 //uysh 项目主入口
 
 mod lib;
+use lib::variable;
 fn main() {
     println!("Hello, world!");
-    println!("{}", lib::START_DATE);
+    // @todo 此处 【variable::variable】 编写不优雅
+    println!("{}", variable::variable::START_DATE);
 }
