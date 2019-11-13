e074810c3b3e4e89b70428e3fc93f40bf34c9c2c
Conero <conero@163.com>
2019-11-13 15:03:26 +0800

    代码发布

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,5 @@
 pub const START_DATE: &'static str = "2019-11-01";
 pub const AUTHOR: &'static str = "Joshua Conero";
 pub const VERSION: &'static str = "0.1.0";
-pub const RELEASE: &'static str = "future";
+pub const RELEASE: &'static str = "2019-11-13";
 pub const PROJECT_CODE: &'static str = "uysh";
--- uymas/src/lib.rs
+++ uymas/src/lib.rs
@@ -2,8 +2,8 @@
 pub const START_DATE: &'static str = "2019-11-01";
 pub const AUTHOR: &'static str = "Joshua Conero";
 pub const VERSION: &'static str = "0.1.0";
-pub const RELEASE: &'static str = "2019-11-10";
-pub const PROJECT: &'static str = "命令行解析程序";
+pub const RELEASE: &'static str = "2019-11-13";
+pub const PROJECT: &'static str = "uymas";
 
 pub mod cmd;
 
